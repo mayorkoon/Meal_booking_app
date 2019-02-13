d0ea9e89fc48c8407841e0819b83ab268c7712cf
mayorkoon <mayorport09@yahoo.com>
2019-02-13 18:51:07 +0100

    Made some changes to enable radio buttons make certain selections and also renamed the html files and their references

---
+++ style.js
@@ -0,0 +1,8 @@
+ let username = document.getElementById('username').value;
+ let password = document.getElementById('password').value;
+ let btn = document.getElementById('butn');
+ if (username == null && password == null)
+ {
+     btn.onclick() = function(){
+     alert('Please Enter username and password');
+ }}
