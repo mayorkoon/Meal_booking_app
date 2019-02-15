570b0f2999d3176d35d50dc13a4ac13be7869df9
mayorkoon <mayorport09@yahoo.com>
2019-02-15 13:14:57 +0100

    changes made to hrefs

--- style.js
+++
@@ -1,8 +0,0 @@
- let username = document.getElementById('username').value;
- let password = document.getElementById('password').value;
- let btn = document.getElementById('butn');
- if (username == null && password == null)
- {
-     btn.onclick() = function(){
-     alert('Please Enter username and password');
- }}
