f78eed50842bab77796caba9630389da39028006
mayorkoon <mayorport09@yahoo.com>
2019-02-16 01:22:10 +0100

    UI modification

---
+++ style.js
@@ -0,0 +1,24 @@
+document.getElementById('butn1').addEventListener("click", function() {
+	document.querySelector('.nw-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.nw-modal').style.display = "none";
+});
+
+function myFunction(){
+    modal.style.display="flex"
+}
+    
+ let button = document.getElementById("butn2");
+ let modal = document.getElementsByClassName('nw-modal');
+ button.onclick = myFunction(); 
+       
+      
+ 
+
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.nw-modal').style.display = "none";
+});
+
