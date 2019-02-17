6fef4939cd92bc54af3cf8593b361a5867e43ad2
mayorkoon <mayorport09@yahoo.com>
2019-02-17 22:54:23 +0100

    Created meal services

--- Server/utilities/dummyMenu.js
+++ Server/utilities/dummyMenu.js
@@ -2,6 +2,55 @@ export default {
     menu: [
         {   
             day: "Monday",
+            category: "Breakfast"
+            meals:  [
+                        {
+                            id: 1,
+                            name: 'Coconut Rice',
+                            size: 'Large',
+                            price: "600"
+                        },
+                        {
+                            id: 2,
+                            name: 'Fried Rice',
+                            size: 'Medium',
+                            price: "700"
+                        },
+                        {
+                            id: 3,
+                            name: 'Ewa agoyin',
+                            size: 'Large',
+                            price: "350"
+                        },
+                    ]
+        },
+        {   
+            day: "Monday",
+            category: "Lunch"
+            meals:  [
+                        {
+                            id: 1,
+                            name: 'Coconut Rice',
+                            size: 'Large',
+                            price: "600"
+                        },
+                        {
+                            id: 2,
+                            name: 'Fried Rice',
+                            size: 'Medium',
+                            price: "700"
+                        },
+                        {
+                            id: 3,
+                            name: 'Ewa agoyin',
+                            size: 'Large',
+                            price: "350"
+                        },
+                    ]
+        },
+        {   
+            day: "Monday",
+            category: "Dinner"
             meals:  [
                         {
                             id: 1,
